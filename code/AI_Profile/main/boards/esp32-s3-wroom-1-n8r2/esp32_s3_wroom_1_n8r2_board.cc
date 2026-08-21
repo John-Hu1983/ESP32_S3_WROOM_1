c2b43b618ecae1a89cc91444bf32bbd5b2c56b3e
johnhu <johnh@spinmaster.com>
2026-08-21 11:30:24 +0800

    create new project

---
+++ code/AI_Profile/main/boards/esp32-s3-wroom-1-n8r2/esp32_s3_wroom_1_n8r2_board.cc
@@ -0,0 +1,31 @@
+#include "wifi_board.h"
+#include "codecs/no_audio_codec.h"
+#include "config.h"
+
+class Esp32S3Wroom1N8r2Board : public WifiBoard {
+public:
+    AudioCodec* GetAudioCodec() override {
+#ifdef AUDIO_I2S_METHOD_SIMPLEX
+        static NoAudioCodecSimplex audio_codec(
+            AUDIO_INPUT_SAMPLE_RATE,
+            AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_SPK_GPIO_BCLK,
+            AUDIO_I2S_SPK_GPIO_LRCK,
+            AUDIO_I2S_SPK_GPIO_DOUT,
+            AUDIO_I2S_MIC_GPIO_SCK,
+            AUDIO_I2S_MIC_GPIO_WS,
+            AUDIO_I2S_MIC_GPIO_DIN);
+#else
+        static NoAudioCodecDuplex audio_codec(
+            AUDIO_INPUT_SAMPLE_RATE,
+            AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_BCLK,
+            AUDIO_I2S_GPIO_WS,
+            AUDIO_I2S_GPIO_DOUT,
+            AUDIO_I2S_GPIO_DIN);
+#endif
+        return &audio_codec;
+    }
+};
+
+DECLARE_BOARD(Esp32S3Wroom1N8r2Board);
